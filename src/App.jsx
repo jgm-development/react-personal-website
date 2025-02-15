2aa9dfae0decfcfd213ef85b5632ee6eb81cc8c6
jay-glenn <jayglenn.manansala@gmail.com>
2025-02-15 17:35:02 +0800

    Added updates
    Added:
    1.) Nav (desktop) and (mobile)
    2.) Starting to add contents in Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/NavBar';
+import { MobileMenu } from './components/MobileMenu';
 import './index.css';
+import { Home } from './components/sections/Home';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}></LoadingScreen>}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}></LoadingScreen>}{" "}
+      <div className={`min-hs-screen transition-opaticity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-green-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+      </div>
     </>
   )
 }
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return <section id="home" className="min-h-screen flex items-center justify-center relative">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-gray-600 to-green-600 bg-clip-text text-transparent leading-right">
+                Hi!, I'm Jay
+            </h1>
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                I am a web developer with over 8 years of experience in web development, specializing in JavaScript, HTML, and CSS. I have a strong background in ASP .NET MVC, ASP .NET Core and MS SQL Server, also particularly with stored procedures, gained from maintaining complex systems in my early career. More recently, I’ve focused on front-end development and am actively enhancing my skills in React and Angular. I’m passionate about creating dynamic, user-friendly interfaces and am excited about opportunities that challenge me to grow further in modern front-end and back end frameworks."
+            </p>
+        </div>
+    </section>
+}
